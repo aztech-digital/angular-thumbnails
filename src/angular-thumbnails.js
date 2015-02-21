b764a716f9ad6b66c017b0053da254991e8a8214
Thibaud Fabre <thibaud@aztech.io>
2015-02-21 01:08:49 +0100

    Add missing args to drawImage

--- src/angular-thumbnails.js
+++ src/angular-thumbnails.js
@@ -122,7 +122,9 @@
           )).adjustCanvasDimensions(img.height, img.width, canvas);
 
           canvas.getContext('2d')
-            .drawImage(img, 0, 0, img.width, img.height, canvas.width, canvas.height);
+            .drawImage(img,
+                    0, 0, img.width, img.height,
+                    0, 0, canvas.width, canvas.height);
         });
       }, false);
 
